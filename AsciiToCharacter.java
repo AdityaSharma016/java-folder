d5249bd70ef9099e2138c9cd19fcf201f69937d6
AdityaSharma016 <adityasharmareal7891@gmail.com>
2025-05-23 15:59:52 +0530

    new

---
+++ AsciiToCharacter.java
@@ -0,0 +1,14 @@
+public class AsciiToCharacter  
+{ 
+ 
+ public static void main(String[] args)  
+ { 
+  char c;    
+  for(int i=65;i<=90;i++)   
+  { 
+   c =(char)i;     
+   System.out.println(i+"  =  "+c);  
+  } 
+ } 
+ 
+}
--- one.java
+++
@@ -1,7 +0,0 @@
-public class one {
-    public static void main(String[] args) {
-
-        int t;
-        Scanner sc = new Scanner(System.in);
-}
-}
